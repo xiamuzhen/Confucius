84696c1dd8a3f2fb1c6c50098b527d9491eabd8c
Xiamuzhen <xiamuzhen@163.com>
2020-05-26 11:21:14 +0800

    Update MySQL.sh

--- MySQL.sh
+++ MySQL.sh
@@ -2,15 +2,15 @@
 name=MySQL 5.6 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/
 enabled=1
-gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+gpgcheck=1
+gpgkey=http://repo.mysql.com/RPM-GPG-KEY-mysql
 
 [mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
 enabled=1
-gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+gpgcheck=1
+gpgkey=http://repo.mysql.com/RPM-GPG-KEY-mysql
 
 yum -y install mysql-community-server
 
