4bbccfebfa567d138e89fb5b805eb7214657a00e
Xiamuzhen <xiamuzhen@163.com>
2020-05-26 11:16:23 +0800

    Update MySQL.sh

--- MySQL.sh
+++ MySQL.sh
@@ -1,7 +1,19 @@
-# Enable to use MySQL 5.6
 [mysql56-community]
 name=MySQL 5.6 Community Server
-baseurl=http://repo.mysql.com/yum/mysql-5.6-community/el/6/$basearch/
+baseurl=http://repo.mysql.com/yum/mysql-5.6-community/el/7/$basearch/
 enabled=1
-gpgcheck=1
+gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+
+[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
+enabled=1
+gpgcheck=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+
+yum -y install mysql-community-server
+
+grep 'temporary password' /var/log/mysqld.log
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'MyNewPass4!';
