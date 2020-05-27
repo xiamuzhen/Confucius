8536de267218af1237fb40cb6a1fd927bb4db546
Xiamuzhen <xiamuzhen@163.com>
2020-05-27 21:26:16 +0800

    Create Hadoop.sh

---
+++ Hadoop.sh
@@ -0,0 +1,46 @@
+cat >> /etc/profile << EOF
+export JAVA_HOME=/root/jdk/
+export HADOOP_HOME=/root/hadoop/
+export PATH=\$PATH:\$JAVA_HOME/bin:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin
+EOF
+source /etc/profile
+
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+
+etc/hadoop/core-site.xml:
+    <property>
+        <name>fs.defaultFS</name>
+        <value>hdfs://localhost:9000</value>
+    </property>
+    <property>
+        <name>hadoop.tmp.dir</name>
+        <value>/tmp/hadoop-${user.name}</value>
+    </property>
+
+etc/hadoop/hdfs-site.xml:
+    <property>
+        <name>dfs.replication</name>
+        <value>1</value>
+    </property>
+    <property>
+        <name>dfs.namenode.name.dir</name>
+        <value>file://${hadoop.tmp.dir}/dfs/name</value>
+    </property>
+    <property>
+        <name>dfs.datanode.data.dir</name>
+        <value>file://${hadoop.tmp.dir}/dfs/data</value>
+    </property>
+
+etc/hadoop/mapred-site.xml:
+    <property>
+        <name>mapreduce.framework.name</name>
+        <value>yarn</value>
+    </property>
+
+etc/hadoop/yarn-site.xml:
+    <property>
+        <name>yarn.nodemanager.aux-services</name>
+        <value>mapreduce_shuffle</value>
+    </property>
