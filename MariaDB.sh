d33e2b5e0a78624e8e435cb193c851f2f6c86a03
Xiamuzhen <xiamuzhen@163.com>
2020-05-28 11:37:29 +0800

    Update MariaDB.sh

--- MariaDB.sh
+++ MariaDB.sh
@@ -1,10 +1,10 @@
 cat > /etc/yum.repos.d/MariaDB.repo << EOF
 [mariadb]
-name = MariaDB
-baseurl = https://mirrors.tuna.tsinghua.edu.cn/mariadb/yum/10.1/centos7-amd64
-enabled = 1
-gpgcheck = 1
-gpgkey = https://mirrors.tuna.tsinghua.edu.cn/mariadb/yum/RPM-GPG-KEY-MariaDB
+name=MariaDB
+baseurl=https://mirrors.tuna.tsinghua.edu.cn/mariadb/yum/10.1/centos7-amd64
+enabled=1
+gpgcheck=1
+gpgkey=https://mirrors.tuna.tsinghua.edu.cn/mariadb/yum/RPM-GPG-KEY-MariaDB
 EOF
 
 yum -y install MariaDB-server MariaDB-client
