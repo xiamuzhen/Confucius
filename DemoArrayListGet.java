87dba320e87dea441582570ae1449ed9cce9fcbf
Xiamuzhen <xiamuzhen@163.com>
2020-05-29 21:52:43 +0800

    Create DemoArrayListGet.java

---
+++ DemoArrayListGet.java
@@ -0,0 +1,32 @@
+
+import java.util.AbstractList;
+
+public class DemoArrayListGet<E> extends AbstractList {
+    private int size;
+
+    @Override
+    public Object get(int index) {
+        rangeCheck(index);
+        return elementData(index);
+    }
+
+    private void rangeCheck(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+ index + ", Size: " + size;
+    }
+
+    E elementData(int index) {
+        return elementData(index);
+    }
+
+}
