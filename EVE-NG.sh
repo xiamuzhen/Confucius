e4687232aabc7967fb44a67bf4c8576ede78d11a
Xiamuzhen <xiamuzhen@163.com>
2020-09-15 20:13:30 +0800

    Create EVE-NG.sh

---
+++ EVE-NG.sh
@@ -0,0 +1,22 @@
+
+/opt/unetlab/addons/dynamips/
+/opt/unetlab/addons/iol/bin/
+/opt/unetlab/wrappers/unl_wrapper -a fixpermissions
+
+cat > /etc/apt/sources.list << EOF
+deb http://mirrors.aliyun.com/ubuntu/ xenial main
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main
+
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main
+
+deb http://mirrors.aliyun.com/ubuntu/ xenial universe
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
+
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security universe
+EOF
